b9ce0919ba5a0a29f8d96d3a4cd8035837ee09b5
Rajak13 <queresma05@gmail.com>
2025-10-09 15:49:11 +0545

    Added Hero section

--- summarize/frontend/app/page.tsx
+++ summarize/frontend/app/page.tsx
@@ -1,7 +1,33 @@
-async function getStrapiData(url: string) {
+import qs from "qs";
+import { HeroSection } from "@/components/custom/hero-section"; // add this
+
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      on: {
+        "layout.hero-section": {
+          populate: {
+            image: {
+              fields: ["url", "alternativeText"],
+            },
+            link: {
+              populate: true,
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
+async function getStrapiData(path: string) {
   const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
   try {
-    const response = await fetch(baseUrl + url);
+    const response = await fetch(url.href);
     const data = await response.json();
     return data;
   } catch (error) {
@@ -11,13 +37,12 @@ async function getStrapiData(url: string) {
 
 export default async function Home() {
   const strapiData = await getStrapiData("/api/home-page");
-
-  const { title, description } = strapiData.data;
+  console.dir(strapiData.data, { depth: null });
+  const { blocks } = strapiData.data; // add this
 
   return (
-    <main className="container mx-auto py-6">
-      <h1 className="text-5xl font-bold">{title}</h1>
-      <p className="text-xl mt-4">{description}</p>
+    <main>
+      <HeroSection data={blocks[0]} />
     </main>
   );
 }
